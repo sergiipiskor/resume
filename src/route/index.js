0d04929dc0d957ee084bfd70d2a7b80214e817a4
sergiipiskor <sergiipiskor@gmail.com>
2023-06-04 19:23:24 +0300

    done 4 lessons

--- src/route/index.js
+++ src/route/index.js
@@ -5,28 +5,257 @@ const router = express.Router()
 
 // ================================================================
 
+var header = {
+    name: {
+        firstname: 'Dmytro',
+        lastname: 'Ivanov',
+    },
+    position: 'Junior Fullstack JS Developer',
+    salary: '600$ в місяц',
+    address: 'Mykolaiv, 6 Slobidska, 43',
+}
+
+var footer = {
+    social: {
+        email: {
+            text: 'dmytro@mail.com',
+            href: 'mailto:dmytro@mail.com',
+        },
+        phone: {
+            text: 'tel:+380670000123',
+            href: '+380670000123',
+        },
+        linkedin: {
+            text: 'LinkedIn',
+            href: 'https://www.linkedin.com/in/dmytro-test',
+        },
+    },
+}
+
+// ================================================================
+
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
 router.get('/', function (req, res) {
-  // res.render генерує нам HTML сторінку
+    // res.render генерує нам HTML сторінку
 
-  //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('index', {})
-  //                  ↑↑ сюди вводимо JSON дані
+    //            ↙ cюди вводимо назву файлу з сontainer
+    res.render('index', {})
+    //                  ↑↑ сюди вводимо JSON дані
 })
 
 // ================================================================
 
 //              ↙ тут вводимо шлях (PATH) до сторінки
 router.get('/summary', function (req, res) {
-  //             ↙ cюди вводимо назву файлу з сontainer
-  res.render('summary', {
-    // ↙ сюди вводимо JSON дані
-  })
+    //             ↙ cюди вводимо назву файлу з сontainer
+    res.render('summary', {
+        // ↙ сюди вводимо JSON дані
+        page: {
+            title: 'Resume | Summary',
+        },
+
+        header,
+
+        main: {
+            summary: {
+                title: 'Summary',
+                text: `Open-minded for new technologies, with 1 years of experience in development. 
+      Whenever I start to work on a new project I learn the domain and try to understand the
+      idea of the project.Good team player, every colleague is a friend to me.`,
+            },
+            experience: {
+                title: 'Other experience',
+                text: `Pet project for parsing sport betting data from different platforms ( odds ) 
+      and sport statistics (tournament position, goals etc), analyzing by simple mathematics
+      models and preparing probability for such events like: money line -
+      first win / draw / second win, totals etc.`,
+            },
+        },
+
+        footer,
+    })
 })
 
 // ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/skills', function (req, res) {
+    //             ↙ cюди вводимо назву файлу з сontainer
+    res.render('skills', {
+        // ↙ сюди вводимо JSON дані
+        page: {
+            title: 'Resume | Skills',
+        },
+
+        header,
+
+        main: {
+            skills: [
+                {
+                    name: 'HTML',
+                    point: 10,
+                    isTop: true,
+                },
+                {
+                    name: 'Handlebars',
+                    point: 10,
+                    isTop: true,
+                },
+                {
+                    name: 'VS Code & NPM',
+                    point: 4,
+                    isTop: false,
+                },
+                {
+                    name: 'Git & Terminal',
+                    point: 9,
+                },
+                {
+                    name: 'React.js',
+                    point: 0,
+                },
+                {
+                    name: 'PHP',
+                    point: null,
+                },
+            ],
+            hobbies: [
+                {
+                    name: 'Footboll',
+                    isMain: true,
+                },
+                {
+                    name: 'Tenis',
+                    isMain: false,
+                },
+                {
+                    name: 'Сomputer games',
+                    isMain: true,
+                },
+            ],
+        },
+
+        footer,
+    })
+})
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/education', function (req, res) {
+    //             ↙ cюди вводимо назву файлу з сontainer
+    res.render('education', {
+        // ↙ сюди вводимо JSON дані
+        page: {
+            title: 'Resume | Education',
+        },
+
+        header,
+
+        main: {
+            educations: [
+                {
+                    name: 'МДАУ',
+                    isEnd: true,
+                },
+                {
+                    name: 'МНАУ',
+                    isEnd: true,
+                },
+                {
+                    name: 'КПУ',
+                    isEnd: false,
+                },
+                {
+                    name: 'СКВА',
+                    isEnd: false,
+                },
+            ],
+            certificates: [
+                {
+                    name: 'Certificate 1',
+                    id: 123,
+                },
+                {
+                    name: 'Certificate 2',
+                    id: 456,
+                },
+                {
+                    name: 'Certificate 3',
+                    id: 789,
+                },
+            ],
+        },
+
+        footer,
+    })
+})
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/work', function (req, res) {
+    //             ↙ cюди вводимо назву файлу з сontainer
+    res.render('work', {
+        // ↙ сюди вводимо JSON дані
+
+        layout: 'big',
+
+        page: {
+            title: 'Resume | Work',
+        },
+
+        header,
+
+        main: {
+            works: [
+                {
+                    position: 'Junior Full Stack Developer',
+                    company: {
+                        name: 'IT Brains',
+                        url: null,
+                    },
+                    durations: {
+                        from: '11.10.2022',
+                        to: null,
+                    },
+                    projectAmount: 3,
+                    projects: [
+                        {
+                            name: 'Resume',
+                            url: 'https://resume.com.ua/',
+                            about: 'Both the Bilstein B6 4600s and B8 5100s shock absorbers can be used with your stock O.E. springs, without requiring any aftermarket hardware',
+                            stacks: [
+                                {
+                                    name: 'React.js',
+                                },
+                                {
+                                    name: 'HTML / CSS',
+                                },
+                                {
+                                    name: 'Node.js',
+                                },
+                            ],
+                            awards: [
+                                {
+                                    name: 'Both the Bilstein B6 4600s and B8 5100s shock absorbers can be used with your stock O.E. springs, without requiring any aftermarket hardware.',
+                                },
+                                {
+                                    name: 'The first are designed for OEM Replacement and stock height vehicles only, while the second are made for both stock height and lifted vehicles.',
+                                },
+                            ],
+                            stackAmount: 3,
+                            awardAmount: 2,
+                        },
+                    ],
+                },
+            ],
+        },
+        footer,
+    })
+})
 
 // Підключаємо роутер до бек-енду
 module.exports = router

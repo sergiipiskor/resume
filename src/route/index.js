0c7ff564c5471977b0ea22ada56935bdf2d80fb3
sergiipiskor <sergiipiskor@gmail.com>
2023-06-19 15:57:20 +0300

    add bootstrap container

--- src/route/index.js
+++ src/route/index.js
@@ -1552,5 +1552,19 @@ router.get('/task31', function (req, res) {
 
 // ================================================================
 
+router.get('/bootstrap', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('bootstrap', {
+    layout: 'basic',
+
+    page: {
+      title: 'Task31',
+    },
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
 // Підключаємо роутер до бек-енду
 module.exports = router
